91d9f7b7bc1852db45fb306dc3ff42ca779f4e01
Marcelleejx <marcelleejx@u.nus.edu>
2024-03-24 17:45:45 +0800

    Added mock data to display itinerary

--- src/components/PlacesToVisit.vue
+++ src/components/PlacesToVisit.vue
@@ -19,7 +19,7 @@
           class="fa-regular calendar-icon"
           :size="iconSize"
         />
-        <h2>Day {{ index + 1 }}</h2>
+        <h2>Day {{ day }}</h2>
         <button
           v-if="index === days.length - 1 && days.length !== 1"
           class="delete-day-button"
@@ -30,10 +30,30 @@
       </div>
 
       <div class="location-container">
-        <button class="plus-button" @click="showAddLocationForm = index + 1">
-          +
-        </button>
-        <span class="add-location">Add Location</span>
+        <div
+          class="location-details"
+          v-for="item in filteredMockData(day)"
+          :key="item"
+        >
+          <div class="location-header">
+            <h3>{{ item.location }}</h3>
+            <div>
+              <span
+                class="location-category"
+                :style="{ backgroundColor: getCategoryColor(item.category) }"
+                >{{ item.category }}</span
+              >
+              <button class="minus-button">-</button>
+            </div>
+          </div>
+          <span class="location-description">{{ item.description }}</span>
+        </div>
+        <div class="add-location-container">
+          <button class="plus-button" @click="showAddLocationForm = day">
+            +
+          </button>
+          <span class="add-location">Add Location</span>
+        </div>
       </div>
     </div>
 
@@ -56,6 +76,7 @@ import AddLocationForm from "./AddLocationForm.vue";
 export default {
   mounted() {
     document.body.style.backgroundColor = "#e7dcdc";
+    this.days = [...new Set(this.mockData.map((item) => item.day))];
   },
 
   beforeDestroy() {
@@ -67,7 +88,71 @@ export default {
     return {
       showAddLocationForm: null,
       iconSize: "xl",
-      days: [1],
+      days: [],
+      mockData: [
+        {
+          day: 1,
+          location: "Marina Bay Sands",
+          description:
+            "Integrated resort (IR) with a hotel, casino, retail mall, as well as convention facilities and entertainment venues including theatres, nightclubs and a museum.",
+          category: "Hotel",
+        },
+        {
+          day: 1,
+          location: "Gardens by the Bay",
+          description:
+            "Gardens by the Bay is a huge, colourful, futuristic park in the bay area of Singapore. Among the standout features are the famous Supertree structures. These offer an impressive skywalk over the gardens, with oversized seashell-shaped greenhouses that recreate chilly mountain climates.",
+          category: "Nature",
+        },
+        {
+          day: 2,
+          location: "PocoLoco",
+          description: "Best Italian restaurant in Singapore",
+          category: "Food",
+        },
+        {
+          day: 2,
+          location: "Atlas",
+          description:
+            "Luxurious 1920s-inspired venue for European cuisine, afternoon tea & cocktails, plus art deco decor.",
+          category: "Bar",
+        },
+        {
+          day: 2,
+          location: "Skyline Luge Sentosa",
+          description:
+            "Skyline Luge Singapore offers 4 purpose-built tracks with hairpin corners, exhilarating tunnels and downhill slopes through a rainforest with mystical creatures, which can be enjoyed during the day or night! It's the ultimate fun day out for the whole family.",
+          category: "Adventure",
+        },
+        {
+          day: 2,
+          location: "Merlion Park",
+          description:
+            "The Merlion is the official mascot of Singapore. It is depicted as a mythical creature with the head of a lion and the body of a fish.",
+          category: "Sightseeing",
+        },
+        {
+          day: 3,
+          location: "ION Orchard",
+          description:
+            "ION Orchard is a stylish architectural wonder, glowing like a futuristic beacon at the end of Orchard Road - enticing fashionistas and luxury shoppers inside.",
+          category: "Shopping",
+        },
+        {
+          day: 3,
+          location: "Sri Mariamman Temple",
+          description:
+            "The oldest Hindu temple in Singapore, Sri Mariamman Temple serves as a focal point for the Hindu community, and is dedicated to Goddess Mariamman, known for her powers in curing epidemic illnesses.",
+          category: "Religious Site",
+        },
+        {
+          day: 3,
+          location: "ABC Cooking Studio",
+          description:
+            "ABC Cooking Studio, which hails from Japan, is one of the country's top culinary schools. With four outlets in Singapore, the classes teach you to master the art of Japanese cuisine, from takoyaki to tempura to tonkatsu.",
+          category: "Others",
+        },
+      ],
     };
   },
 
@@ -76,7 +161,27 @@ export default {
       this.days.push(this.days.length + 1);
     },
     deleteDay(index) {
+      // add code to delete data from firebase with itineraries in the same day
       this.days.splice(index, 1);
+    },
+    filteredMockData(dayNumber) {
+      return this.mockData.filter((item) => item.day === dayNumber);
+    },
+    getCategoryColor(category) {
+      // Define colors for different categories
+      const categoryColors = {
+        Food: "#FF6B6B",
+        Bar: "#F28E1C",
+        Adventure: "#593233",
+        Hotel: "#4E7FE0",
+        Nature: "#4EE0A3",
+        Sightseeing: "#854DDC",
+        Shopping: "#92B1B6",
+        "Religious Site": "#DFBD69",
+        Others: "#808080",
+      };
+      // Return the color for the specified category
+      return categoryColors[category];
     },
   },
 
@@ -155,6 +260,52 @@ h2 {
 }
 
 .location-container {
+  padding-left: 2rem;
+  align-items: center;
+}
+
+.location-details {
+  background-color: rgba(128, 128, 128, 0.25);
+  border-radius: 10px;
+  text-align: left;
+  padding: 1rem;
+  margin-bottom: 1rem;
+}
+
+.location-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 0.5rem;
+}
+
+h3 {
+  margin-top: 0;
+  margin-bottom: 0;
+}
+
+.location-category {
+  padding-top: 0.5rem;
+  padding-bottom: 0.5rem;
+  padding-left: 2rem;
+  padding-right: 2rem;
+  font-size: 14px;
+  border-radius: 10px;
+  color: white;
+}
+
+.minus-button {
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: #808080;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+  margin-left: 0.5rem;
+}
+
+.add-location-container {
   padding-left: 2rem;
   display: flex;
   align-items: center;
